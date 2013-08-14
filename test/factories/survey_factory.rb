957aca4150dd97a9d76d8b44b0831a11d8ca787d
Jenkins <tech@theodi.org>
2013-08-14 01:06:36 +0000

    Refactored survey.requirements and survey.mandatory_questions & wrote tests for them. All tests passing

---
+++ test/factories/survey_factory.rb
@@ -0,0 +1,49 @@
+FactoryGirl.define do
+
+  factory :survey do |s|
+    meta_hash = {
+        dataset_title: 'testDataTitle', 
+        dataset_curator: 'testPublisher',
+        dataset_documentation_url: 'testDocumentationUrl'
+      }
+    
+    s.title "Simple survey"
+    s.description "A simple survey for testing"
+    s.access_code { FactoryGirl.generate :unique_survey_access_code }
+    s.survey_version 0
+    s.meta_map meta_hash
+  
+    after(:create) do |survey, evaluator|
+      FactoryGirl.create_list(:survey_section, 3, survey: survey)
+    end
+
+    factory :survey_with_mandatory_question do |survey|
+      after(:create) do |survey, evaluator|
+        FactoryGirl.create(:survey_section_with_mandatory_question, survey: survey)
+      end
+    end
+
+    factory :survey_with_required_question do |survey|
+      after(:create) do |survey, evaluator|
+        FactoryGirl.create(:survey_section_with_required_question, survey: survey)
+      end
+    end
+
+  end
+
+  sequence :unique_survey_access_code do |n|
+    "simple_survey_#{UUIDTools::UUID.random_create.to_s}"
+  end
+
+
+  factory :survey_translation do |t|
+    t.locale "es"
+    t.translation %(title: "Un idioma nunca es suficiente"
+      survey_sections:
+        one:
+          title: "Uno"
+      questions:
+        hello:)
+  end
+
+end
